682d8f2092254c56dc2802faca233800f81ac923
Sergey Egorov <ser.12egorov@gmail.com>
2021-11-06 18:32:57 +0300

    add AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,11 @@
 
 namespace App\Providers;
 
+use Illuminate\Auth\Notifications\VerifyEmail;
+use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -23,6 +28,26 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        JsonResource::withoutWrapping();
+
+        VerifyEmail::toMailUsing(function ($notifiable) {
+
+            // Генерация ссылки для подтверждения письма
+            $verifyUrl = URL::temporarySignedRoute(
+                'verification.verify', Carbon::now()->addMinutes(60), [
+                    'id' => $notifiable->getKey(),
+                    'hash' => sha1($notifiable->getEmailForVerification())
+                ]
+            );
+
+            // Переменные, которые будут доступны в шаблоне письма
+            $vars = [
+                'url' => $verifyUrl
+            ];
+
+            return (new MailMessage)
+                ->subject('Подтверждение почты') // Тема письма
+                ->markdown('emails.verify', $vars); // Шаблон письма
+        });
     }
 }
