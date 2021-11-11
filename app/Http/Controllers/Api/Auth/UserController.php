001a3e9f19a555197c186b81a44ed1b68bafd5b4
Sergey Egorov <ser.12egorov@gmail.com>
2021-11-11 09:36:13 +0300

    Changes by Sergios

--- app/Http/Controllers/Api/Auth/UserController.php
+++ app/Http/Controllers/Api/Auth/UserController.php
@@ -30,7 +30,7 @@ class UserController extends Controller
 
         return [
             'user' => $user->name,
-            'action' => 'delete',
+            'action' => 'deleted',
             'status' => 'success'
         ];
     }
