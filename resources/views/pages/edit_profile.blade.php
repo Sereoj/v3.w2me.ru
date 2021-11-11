a2de7706f16618cfa80e67d1fdb8ea11a03ec012
Sergey Egorov <ser.12egorov@gmail.com>
2021-11-11 09:38:27 +0300

    fix error results

--- resources/views/pages/edit_profile.blade.php
+++ resources/views/pages/edit_profile.blade.php
@@ -11,25 +11,23 @@
 {{--                            id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus--}}
                             <input id="username" name="name" type="text" class="form-control @error('name') is-invalid @enderror" placeholder="Username" value="{{ $user->name }}" required autofocus>
                         </div>
-                        <div class="invalid-feedback">@error('email'){{$message}}@enderror</div>
+                        <div class="badge bg-danger">@error('name'){{$message}}@enderror</div>
                     </div>
-
                     <div class="col-sm-8">
                         <label for="image" class="form-label">Изображение</label>
                         <input type="file" class="form-control @error('photo') is-invalid @enderror" id="image" name="photo" value="{{ old('photo') }}">
                         <div class="badge bg-danger text-wrap">@error('photo'){{$message}}@enderror</div>
                     </div>
-
                     <div class="col-sm-6">
                         <label for="old-pass" class="form-label">Старый пароль</label>
                         <input type="password" class="form-control @error('old_password') is-invalid @enderror" id="old-pass" name="old_password" min="6">
-                        <div class="invalid-feedback">@error('old_password'){{$message}}@enderror</div>
+                        <div class="badge bg-danger text-wrap">@error('old_password'){{$message}}@enderror</div>
                     </div>
 
                     <div class="col-sm-6">
                         <label for="new-pass" class="form-label">Новый пароль</label>
                         <input type="password" class="form-control @error('new_password') is-invalid @enderror" id="new-pass" name="new_password" min="6">
-                        <div class="invalid-feedback">@error('new_password'){{ $message }}@enderror</div>
+                        <div class="badge bg-danger text-wrap">@error('new_password'){{$message}}@enderror</div>
                     </div>
                     @if($status_image)
                         <div class="alert alert-success" role="alert">Изображение загружено на сервер</div>
