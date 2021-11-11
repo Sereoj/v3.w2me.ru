8b3530a9f7dae2f392be8283399c8d8dabf8e80d
Sergey Egorov <ser.12egorov@gmail.com>
2021-11-11 09:37:58 +0300

    fix destroy

--- app/Http/Controllers/Api/Auth/UserController.php
+++ app/Http/Controllers/Api/Auth/UserController.php
@@ -26,6 +26,10 @@ class UserController extends Controller
 
     public function destroy(User $user)
     {
+        $user->type()->delete();
+        $user->role()->delete();
+        $user->photo()->delete();
+
         $user->delete();
 
         return [
