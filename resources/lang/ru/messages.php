46b12a69ea590a1e7e0ba1c6cfdebfb9bd796d67
Sergey Egorov <ser.12egorov@gmail.com>
2021-11-13 20:15:46 +0300

    add translate

---
+++ resources/lang/ru/messages.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+    'sunrise' => 'Рассвет',
+    'day' => 'День',
+    'sunset' => 'Закат',
+    'night' => 'Ночь',
+
+    'administrator' => 'Администратор',
+    'moderator' => 'Модератор',
+    'user' => 'Пользователь',
+
+    'user_type.free' => 'Бесплатный',
+    'user_type.pay' => 'Платный, действует до:',
+    'balance' => ":cost руб."
+];
