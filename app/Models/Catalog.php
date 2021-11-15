6d3f5c79606a2d64dd67af0019bcb41de0a55162
Sergey Egorov <ser.12egorov@gmail.com>
2021-11-15 19:53:37 +0300

    add functions for Catalog.php

--- app/Models/Catalog.php
+++ app/Models/Catalog.php
@@ -36,4 +36,30 @@ class Catalog extends Model
     use HasFactory;
 
     protected $table = 'catalog';
+    protected $fillable = ['id' , 'name', 'description', 'preview', 'images', 'category_id', 'license_type_id', 'user_id', 'catalog_download_id', 'catalog_rating_id'];
+
+    public function category()
+    {
+        return $this->belongsTo(Categories::class);
+    }
+
+    public function license()
+    {
+        return $this->hasOne(license_type::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function download()
+    {
+        return $this->hasMany(catalog_download::class);
+    }
+
+    public function rating()
+    {
+        return $this->hasMany(catalog_rating::class);
+    }
 }
